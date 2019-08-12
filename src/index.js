1df119501fa2035b23f4ef515d0b65a9c673dae2
Andrea Soloko <andreasoloko@gmail.com>
2019-08-12 11:49:48 -0500

    map feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("running")
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic29sb2tvIiwiYSI6ImNqejhtajN2NDA0NmwzYm1wZWlzdnZ2MnMifQ.ZkZXof3id4PvaIG9Frpjwg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
+
+
